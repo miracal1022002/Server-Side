497033fabf26e963887f3a0dd2b9d774a3951a59
nguyenleanh <leanh1022002@gmail.com>
2023-07-11 23:59:32 +0700

    Adjusted server code

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,23 +2,43 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 class Program
-{
-    static Listener listener;
-    static List<Socket> sockets;
+{ 
     static void Main(string[] args)
     {
-        listener = new Listener(10);
-        listener.SocketAccepted += new Listener.SocketAcceptedHandler(listener_SocketAccepted);
+        TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 1302);
         listener.Start();
-        sockets = new List<Socket>();
-        Console.Read();
-    }
-
-    static void listener_SocketAccepted(System.Net.Sockets.Socket e)
-    {
-        Console.WriteLine("New Connection: {0}\n{1}\n========", e.RemoteEndPoint, DateTime.Now);
-        sockets.Add(e);
+        while (true)
+        {
+            Console.WriteLine("Wating for a connection");
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client accepted");
+            NetworkStream stream = client.GetStream();
+            StreamReader sr = new StreamReader(client.GetStream());
+            StreamWriter sw = new StreamWriter(client.GetStream());
+            try
+            {
+                byte[] buffer = new byte[1024];
+                stream.Read(buffer, 0, buffer.Length);
+                int receive = 0;
+                foreach(byte b in buffer)
+                {
+                    if(b != 0)
+                    {
+                        receive++;
+                    }
+                }
+                string request = Encoding.UTF8.GetString(buffer, 0, receive);
+                Console.WriteLine("Request received: " + request);
+                sw.WriteLine("Response from server");
+                sw.Flush();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
