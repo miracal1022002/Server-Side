079715fb31996c868cd464d5bb78823decaa6a7e
nguyenleanh <leanh1022002@gmail.com>
2023-07-11 14:16:38 +0700

    server side setup

---
+++ Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        s.Connect("127.0.0.1", 3306);
+        s.Close();
+        s.Dispose();
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using Server;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static Listener listener;
+    static List<Socket> sockets;
+    static void Main(string[] args)
+    {
+        listener = new Listener(10);
+        listener.SocketAccepted += new Listener.SocketAcceptedHandler(listener_SocketAccepted);
+        listener.Start();
+        sockets = new List<Socket>();
+        Console.Read();
+    }
+
+    static void listener_SocketAccepted(System.Net.Sockets.Socket e)
+    {
+        Console.WriteLine("New Connection: {0}\n{1}\n========", e.RemoteEndPoint, DateTime.Now);
+        sockets.Add(e);
+    }
+}
